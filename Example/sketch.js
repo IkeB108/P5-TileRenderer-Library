f4459d3e545b09731df9a51bbe5c8c767a64e78e
Ikebot <56776763+IkeB108@users.noreply.github.com>
2022-07-24 17:03:39 -0700

    Update sketch.js

--- Example/sketch.js
+++ Example/sketch.js
@@ -1,21 +1,21 @@
 function preload(){
   //Load a json file that was exported from https://lvllvl.com/
   //( you can also make one from scratch without LVLLVL )
-  myMapJSON = loadJSON("myMap.json")
+  myLvlLvlJSON = loadJSON("LvlLvlExport.json")
 }
 
 function setup(){
   createCanvas(500,500)
   
   //Create a new tile renderer using the json from https://lvllvl.com/
-  myMap = new TileRenderer(myMapJSON);
+  myTileRenderer = new TileRenderer(myLvlLvlJSON);
   
   //The tile renderer automatically creates layer objects from the json data
   //and stores them in an array called layers.
   //We will create a graphic using the data from each layer.
-  for(let i in myMap.layers){
-    let thisLayer = myMap.layers[i];
-    myMap.getGraphic(thisLayer.label, thisLayer) //a graphic is created and added to myMap's array called graphics
+  for(let i in myTileRenderer.layers){
+    let thisLayer = myTileRenderer.layers[i];
+    myTileRenderer.getGraphic(thisLayer.label, thisLayer) //a graphic is created and added to myTileRenderer's array called graphics
   }
   
   frameRateText = "The framerate is " + round(frameRate());
@@ -31,31 +31,30 @@ function draw(){
   scale(2); noSmooth();
   
   
-  // Draw a light green background behind the map graphic
+  // Draw a light green background behind the map
   // ===============================================
-  fill( myMap.colorPalette[0] )
-  // Get the dimensions of a graphic from myMap (doesn't matter which, they're all the same)
-  let w = myMap.graphics["Triangle Background"].width
-  let h = myMap.graphics["Triangle Background"].height
+  fill( myTileRenderer.colorPalette[0] )
+  // Get the dimensions of a graphic from myTileRenderer (doesn't matter which, they're all the same)
+  let w = myTileRenderer.graphics["Triangle Background"].width
+  let h = myTileRenderer.graphics["Triangle Background"].height
   rect(0, 0, w, h)
   
   
-  // Draw each graphic from myMap to the screen
+  // Draw each graphic from myTileRenderer to the screen
   // ===============================================
-  myMap.setGraphicsToUnused(); //This feature will later help delete unused graphics
+  myTileRenderer.setGraphicsToUnused(); //This feature will later help delete unused graphics
   
-  for(let i in myMap.graphics){
-    let thisGraphic = myMap.graphics[i];
+  for(let i in myTileRenderer.graphics){
+    let thisGraphic = myTileRenderer.graphics[i];
     if(!thisGraphic.name.includes("FRAMERATE")){ //We don't yet want to draw the framerate graphic (see below)
       thisGraphic.update();
       image(thisGraphic, 0, 0)
     }
   }
   
-  
   //Create a text graphic that displays the framerate
   //=========================
-  myMap.alphabet = "Commodore 64" //Tell the tile renderer which tileset we used in lvllvl.com so that it knows which tile indeces correspond to which letters (only needs to be done once)
+  myTileRenderer.alphabet = "Commodore 64" //Tell the tile renderer which tileset we used in lvllvl.com so that it knows which tile indeces correspond to which letters (only needs to be done once)
   let myTextSettings = {
     textColor: 3, //text color must be an index from the color palette when using lvllvl.com
     widthInCharacters: 10, //text will wrap after 10 characters
@@ -66,9 +65,9 @@ function draw(){
     frameRateText = "The framerate is " + round(frameRate())
     frameRateText = frameRateText.toUpperCase(); //With the Commodore 64 tileset, all letters must be uppercase. This varies depending on the tileset.
   }
-  myTextGraphic = myMap.getTextGraphic(frameRateText, myTextSettings)
+  myTextGraphic = myTileRenderer.getTextGraphic(frameRateText, myTextSettings)
   myTextGraphic.update();
   image(myTextGraphic, 90, 180 )
   
-  myMap.deleteUnusedGraphics(); //This feature helps clear out unused graphics (especially important when creating text graphics).
+  myTileRenderer.deleteUnusedGraphics(); //This feature helps clear out unused graphics (especially important when creating text graphics).
 }
